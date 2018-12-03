19bf16800d7be42f0d93163f4d63af61b5baf1fa
Sean Griffin <griffin200@hotmail.com>
2018-12-03 20:50:59 +0000

    Console error readings Maj help!!

---
+++ DemoApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoApplication  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Your First Name: ");
+            string FirstName = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter Your Last Name: ");
+
+            Console.ReadLine();
+        }
+    }
+}
