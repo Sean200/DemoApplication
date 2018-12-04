384f07407a6921f94a0fbdb7849e4565dd2699f8
Sean Griffin <griffin200@hotmail.com>
2018-12-04 20:30:14 +0000

    Frustrating as hell! The evenings too short.

--- DemoApplication/Program.cs
+++ DemoApplication/Program.cs
@@ -4,12 +4,18 @@ namespace DemoApplication
 {
     class Program
     {
+        public static string LastName { get; private set; }
+        public static string FirstNameLastName { get; private set; }
+
         static void Main(string[] args)
         {
 
             Console.Write("Enter Your First Name: ");
             string FirstName = Convert.ToString(Console.ReadLine());
             Console.Write("Enter Your Last Name: ");
+            String str = "Hello firstname lastname";
+
+            Console.WriteLine("Hello " + FirstNameLastName);
 
             Console.ReadLine();
             Console.ReadKey(); // use this to stop console from exiting
